2ae04d4b984a1a2ba971e5d485d285fee7663a44
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2022-11-20 23:45:46 +0100

    Actualizo Angular CLI (14.2.9 -> 15.0.0), Angular (14.2.10 -> 15.0.0), Angular Material (14.2.7 -> 15.0.0) y Angular CDK (14.2.7 -> 15.0.0). Correcciones por cambios de diseño.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,56 +1,59 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, LOCALE_ID } from '@angular/core';
-import { registerLocaleData } from '@angular/common';
-import { AppRoutingModule } from 'src/app/app-routing.module';
-import { AppComponent } from 'src/app/app.component';
-import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { environment } from 'src/environments/environment';
+import { registerLocaleData } from "@angular/common";
+import { LOCALE_ID, NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppRoutingModule } from "src/app/app-routing.module";
+import { AppComponent } from "src/app/app.component";
 
-import { TokenInterceptor } from 'src/app/interceptors/token.interceptor';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { MatFormFieldDefaultOptions, MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import {
+  MatFormFieldDefaultOptions,
+  MAT_FORM_FIELD_DEFAULT_OPTIONS,
+} from "@angular/material/form-field";
+import { TokenInterceptor } from "src/app/interceptors/token.interceptor";
 
-import { PAGES, COMPONENTS, PIPES, SERVICES, MATERIAL } from 'src/app/app.common';
+import {
+  COMPONENTS,
+  MATERIAL,
+  PAGES,
+  PIPES,
+  SERVICES,
+} from "src/app/app.common";
 
 const appearance: MatFormFieldDefaultOptions = {
-	appearance: 'outline'
+  appearance: "outline",
 };
 
-import localeEs from '@angular/common/locales/es';
+import localeEs from "@angular/common/locales/es";
 registerLocaleData(localeEs);
 
 @NgModule({
-	declarations: [
-		AppComponent,
-		...PAGES,
-		...COMPONENTS,
-		...PIPES
-	],
-	imports: [
-		BrowserModule,
-		AppRoutingModule,
-		HttpClientModule,
-		FormsModule,
-		BrowserAnimationsModule,
-		...MATERIAL
-	],
-	providers: [
-		{
-			provide: LOCALE_ID,
-			useValue: 'es-ES'
-		},
-		{
-			provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
-			useValue: appearance
-		},
-		...SERVICES,
-		{
-			provide: HTTP_INTERCEPTORS,
-			useClass: TokenInterceptor,
-			multi: true
-		}
-	],
-	bootstrap: [AppComponent]
+  declarations: [AppComponent, ...PAGES, ...COMPONENTS, ...PIPES],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ...MATERIAL,
+  ],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: "es-ES",
+    },
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: appearance,
+    },
+    ...SERVICES,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true,
+    },
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
